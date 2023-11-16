7f0bb2718a766ccffe2c7fe3f694d2975a3509a5
Lim-Heng <www.henglim99@gmail.com>
2023-11-16 15:56:50 +0700

    upate and delete methods

--- modules/report/controller.py
+++ modules/report/controller.py
@@ -13,4 +13,8 @@ router = APIRouter(
 
 @router.post('/create', summary=None, name='POST', operation_id='create')
 def create(request: ReportModel, db: Session = Depends(get_db)):
-    return ReportRepo.create(request, db)
+    return ReportRepository.create(request, db)
+
+@router.delete('/delete/{id}', summary=None, name='DELETE', operation_id='delete')
+def remove(id: int, request: ReportModel, db: Session = Depends(get_db)):
+    return ReportRepository.remove(id, request, db)
--- modules/report/repository.py
+++ modules/report/repository.py
@@ -2,6 +2,7 @@ from core import BaseRepo
 from sqlalchemy.orm import Session
 from .entity import ReportEntity
 from .model import ReportModel
+from fastapi import HTTPException, status
 
 class ReportRepository(BaseRepo):
 
@@ -12,3 +13,21 @@ class ReportRepository(BaseRepo):
         db.commit()
         db.refresh(new_report)
         return new_report
+    
+    @staticmethod
+    def remove(id: int, db: Session):
+        report = db.query(ReportEntity).filter(ReportEntity.id == id)
+        if not report.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Report with id {id} not found')
+        report.delete(synchronize_session=False)
+        db.commit()
+        return 'Deleted sucessfully'
+    
+    @staticmethod
+    def update(id: int, request: ReportModel, db: Session):
+        report = db.query(ReportEntity).filter(ReportEntity.id == id)
+        if not report.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Report with id {id} not found')
+        report.update({'category':request.category, 'priority':request.priority, 'header':request.header, 'information':request.information, 'view':request.view})
+        db.commit()
+        return 'Updated successfully'
