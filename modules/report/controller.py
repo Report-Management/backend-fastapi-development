7d4600c29cd450ae5f62da0f3a796c85613a24bc
Phanith-LIM <limphanith.dev@gmail.com>
2024-01-05 02:02:16 +0700

    [FIX]: Fix type int to str in remove

--- modules/report/controller.py
+++ modules/report/controller.py
@@ -281,7 +281,7 @@ def upload_file(id: str, file: UploadFile, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error.")
 
 @router.delete('/delete_report/{id}', name='DELETE', operation_id='delete_report')
-def remove(id: int, db: Session = Depends(get_db)):
+def remove(id: str, db: Session = Depends(get_db)):
     if id is not type(str):
         HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request")
     try:
